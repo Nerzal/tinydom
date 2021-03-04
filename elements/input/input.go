ac2a7614cdc0e00fdeb5d4089a6d79a4833a1e1d
Tobias Theel <tt@fino.digital>
2021-03-04 22:17:55 +0100

    move input types to own file

--- elements/input/input.go
+++ elements/input/input.go
@@ -2,34 +2,6 @@ package input
 
 import "github.com/Nerzal/tinydom"
 
-type InputType string
-
-const (
-	Password      InputType = "password"
-	ButtonInput   InputType = "button"
-	CheckboxInput InputType = "checkbox"
-	ColorInput    InputType = "color"
-	DateInput     InputType = "date"
-	LocalInput    InputType = "datetime-local"
-	EmailInput    InputType = "email"
-	FileInput     InputType = "file"
-	HiddenInput   InputType = "hidden"
-	ImageInput    InputType = "image"
-	MonthInput    InputType = "month"
-	NumberInput   InputType = "number"
-	PasswordInput InputType = "password"
-	RadioInput    InputType = "radio"
-	RangeInput    InputType = "range"
-	ResetInput    InputType = "reset"
-	SearchInput   InputType = "search"
-	SubmitInput   InputType = "submit"
-	TelInput      InputType = "tel"
-	TextInput     InputType = "text"
-	TimeInput     InputType = "time"
-	UrlInput      InputType = "url"
-	WeekInput     InputType = "week"
-)
-
 type Input struct {
 	*tinydom.Element
 }
